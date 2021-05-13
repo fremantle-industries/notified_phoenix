b22772db44282f09865d5524fb653461033cf7ce
Alex <alex+git@fremantle.io>
2021-05-13 15:26:54 -0700

    WIP: Add tests and documentation
    TODO:
    
    - Add live view tests
    - Add images for components
    - Add speech receiver
    - Add browser notification receiver

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule NotifiedPhoenix.MixProject do
 
   defp deps do
     [
-      {:notified, "~> 0.0.2"},
+      # {:notified, "~> 0.0.2"},
+      {:notified, github: "fremantle-industries/notified", branch: "main"},
       {:phoenix_html, "~> 2.14"},
       {:phoenix_live_view, "~> 0.15"},
       {:ex_machina, "~> 2.7", only: [:dev, :test]},
--- test/notified_phoenix_test.exs
+++ test/notified_phoenix_test.exs
@@ -1,4 +1,24 @@
 defmodule NotifiedPhoenixTest do
   use NotifiedPhoenix.ViewCase
   doctest NotifiedPhoenix
+
+  describe ".render/2 badge.html" do
+    test "displays the count of unread notifications" do
+      html = render(NotifiedPhoenix, "badge.html", unseen: 5)
+
+      assert assert_html(html, "span", text: "5")
+    end
+  end
+
+  describe ".render/2 list.html" do
+    test "displays the details of each notification" do
+      notification_1 = insert(:notification, %{message: "Order has been created"})
+      notification_2 = insert(:notification, %{message: "Order has been shipped"})
+      items = [notification_1, notification_2]
+      html = render(NotifiedPhoenix, "list.html", items: items)
+
+      assert assert_html(html, "ul li:nth-child(1) h4", text: "Order has been created")
+      assert assert_html(html, "ul li:nth-child(2) h4", text: "Order has been shipped")
+    end
+  end
 end
